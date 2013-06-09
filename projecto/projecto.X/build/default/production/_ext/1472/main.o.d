952951f9a0f2b5be10cf08547e1292a5b4d299d9
Andre Viana <sou_eu@crawler.com>
2013-06-09 03:36:07 +0100

    + ‘projecto/projecto.X/.map’ + ‘projecto/projecto.X/build/default/production/_ext/1472/’ + ‘projecto/projecto.X/build/default/production/_ext/1472/main.o’ + ‘projecto/projecto.X/build/default/production/_ext/1472/main.o.d’ + ‘projecto/projecto.X/dist/default/production/projecto.X.production.cof’ + ‘projecto/projecto.X/dist/default/production/projecto.X.production.hex’ + ‘projecto/projecto.X/main.err’

---
+++ projecto/projecto.X/build/default/production/_ext/1472/main.o.d
@@ -0,0 +1,8 @@
+build/default/production/_ext/1472/main.o:  \
+	/home/andre/SparkleShare/miniprojecto/projecto/main.c  \
+	/opt/microchip/mplabc18/v3.40/h/usart.h  \
+	/opt/microchip/mplabc18/v3.40/h/adc.h  \
+	/opt/microchip/mplabc18/v3.40/h/p18f4580.h  \
+	/opt/microchip/mplabc18/v3.40/h/pconfig.h  \
+	/opt/microchip/mplabc18/v3.40/h/p18cxxx.h  \
+
