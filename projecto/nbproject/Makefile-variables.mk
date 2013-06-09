50af36bfea639d5e7c7394238f457cb828bbe819
Andre Viana <sou_eu@crawler.com>
2013-06-09 03:26:02 +0100

    + ‘projecto/Makefile’ + ‘projecto/nbproject/’ + ‘projecto/nbproject/Makefile-default.mk’ + ‘projecto/nbproject/Makefile-genesis.properties’ + ‘projecto/nbproject/Makefile-impl.mk’ + ‘projecto/nbproject/Makefile-local-default.mk’ + ‘projecto/nbproject/Makefile-variables.mk’ + ‘projecto/nbproject/Package-default.bash’ + ‘projecto/nbproject/configurations.xml’ + ‘projecto/nbproject/private/configurations.xml’ ...

---
+++ projecto/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=projecto.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/projecto.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=projecto.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/projecto.tar
