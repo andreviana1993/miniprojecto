22af4e9af18e71998f5ba5e2848ec93cc9eb3a6a
André Viana <sou_eu@crawler.com>
2013-06-14 18:44:19 +0100

    / ‘projecto/main.c’ / ‘projecto/main.err’

--- projecto/main.c
+++ projecto/main.c
@@ -1,5 +1,4 @@
 #include <p18f4580.h>
-#include <usart.h>
 #include <adc.h>
 #include <timers.h>
 
@@ -7,28 +6,26 @@
 
 int resultado;
 
-
 /******************************/
-
 void low_ISR(void);
 void high_ISR(void);
-  
-  
+
+
 #pragma code high_vector = 0x08 // force the following statement to start
 void high_interrupt (void) // at 0x08
 {
-    _asm
-    goto high_ISR
-    _endasm
+	_asm
+	goto high_ISR
+	_endasm
 }
-  
+
 #pragma code low_vector = 0x18 //force the following statements to start
 void low_interrupt (void) // at 0x18
 {
-    _asm
-    goto low_ISR
-    _endasm
-} 
+	_asm
+	goto low_ISR
+	_endasm
+}
 
 /*****************************/
 
@@ -36,84 +33,60 @@ void low_interrupt (void) // at 0x18
 #pragma interrupt high_ISR
 void high_ISR (void)
 {
-    if(INTCONbits.TMR0IF) //handle high-priority interrupts
-        {
-            OpenADC( ADC_FOSC_64 & ADC_RIGHT_JUST & ADC_1ANA , ADC_CH0 & ADC_INT_OFF , 0 );
-            ConvertADC();
-            while(busyADC());
-            resultado= ReadADC();
-//
-            OpenTimer0( TIMER0_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_64 );
-            WriteTimer0( 0x7A & 0xE1 );
+	if(INTCONbits.TMR0IF) //handle high-priority interrupts
+		{
+			OpenADC( ADC_FOSC_64 & ADC_RIGHT_JUST & ADC1_ANA , ADC_CH0 & ADC_INT_OFF , 0 );
+			ConvertADC();
+			while(busyADC());
+			resultado= ReadADC();
 
- 	    //	   TMR0H = 0x67;
-    //		   TMR0L = 0x69;
-            INTCONbits.TMR0IF=0;
-        }
+			OpenTimer0( TIMER0_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_64 );
+			WriteTimer0( 0x7A & 0xE1 );
+			INTCONbits.TMR0IF=0;	
+		}
 }
-  
+
+
 #pragma interruptlow low_ISR
 void low_ISR (void)
 {
-    _asm // handle low-priority interrupts
-    retfie 0 // simply return
-    _endasm
-} 
-
-
-/*****************************/
-
-unsigned char getc_usart(void) {
-    while (!PIR1bits.RCIF);
-    PIR1bits.RCIF = 0;
-    return RCREG;
+	_asm // handle low-priority interrupts
+	retfie 0 // simply return
+	_endasm
 }
 
 /*********************************/
 
-void main(void) {
+ void main (void)
+{
 
-    char c;
-    char str[7] = "ECHO:x\0";
+	RCONbits.IPEN = 1; // Enable priority interrupt
+	INTCON = 0b10100000;
 
-    RCONbits.IPEN = 1; // Enable priority interrupt
-    INTCON = 0b10100000;
+	TRISD = 0x00; // Configure PORTD for output
+	PORTD = 0b00000000; // turn off all LEDs initially
+	
+	TMR0H=0x67;
+	TMR0L=0x69;
+	T0CON=0b10000110;
 
-    TRISD = 0b00000000; // Configure PORTD for output
-    PORTD = 0b00000000; // turn off all LEDs initially
-    TRISB = 0b00000000;
-    PORTB = 0b00000000;
+	OpenADC( ADC_FOSC_64 & ADC_RIGHT_JUST & ADC1_ANA , ADC_CH0 & ADC_INT_OFF , 0 );
+	OpenTimer0( TIMER0_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_64 );
+	WriteTimer0( 0x7A & 0xE1 );
+	
 
-    TMR0H = 0x67;
-    TMR0L = 0x69;
-    T0CON = 0b10000110;
+	//CONFIGURAR AQUI AS INTERRUPÇÕES
 
-//    configure USART
-    OpenUSART(USART_TX_INT_OFF &
-            USART_RX_INT_OFF &
-            USART_ASYNCH_MODE &
-            USART_EIGHT_BIT &
-            USART_CONT_RX,
-            129);
-
-    OpenADC( ADC_FOSC_64 & ADC_RIGHT_JUST & ADC1_ANA_2REF , ADC_CH0 & ADC_INT_OFF , 0 );
-    OpenTimer0( TIMER0_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_64 );
-    WriteTimer0( 0x7A & 0xE1 );
-
-
-
-    while (1) // stay in an infinite loop
-    {
-        c = getc_usart();
-        if (c == 'l') {
-            PORTB = 0b00000001;
-        } else if (c == 'd') {
-            PORTB = 0b00000000;
-        }
-
-
-        // get char from USART by polling method
-        str[5] = c;
-        putsUSART(str); // send string to USART
-    }
+	while (1); // stay in an infinite loop
+	{
+		if     ( resultado >= 0   && resultado < 127)   PORTD = 0b00000001;
+		else if( resultado >= 127 && resultado < 255)   PORTD = 0b00000010;
+		else if( resultado >= 255 && resultado < 383)   PORTD = 0b00000100;
+		else if( resultado >= 383 && resultado < 511)   PORTD = 0b00001000;
+		else if( resultado >= 511 && resultado < 639)   PORTD = 0b00010000;
+		else if( resultado >= 639 && resultado < 767)   PORTD = 0b00100000;
+		else if( resultado >= 767 && resultado < 895)   PORTD = 0b01000000;
+		else if( resultado >= 895 && resultado < 1023)  PORTD = 0b10000000;
+		else  										    PORTD = 0b00000000;
+	}
 }
