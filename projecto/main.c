236b4c647401287cade68fea04537a84a908db93
André Viana <sou_eu@crawler.com>
2013-06-16 17:35:56 +0100

    / ‘projecto/main.c’ / ‘projecto/projecto.X/.map’ / ‘projecto/projecto.X/build/default/production/_ext/1472/main.o’ / ‘projecto/projecto.X/dist/default/production/projecto.X.production.cof’

--- projecto/main.c
+++ projecto/main.c
@@ -13,47 +13,49 @@ void low_ISR(void);
 void high_ISR(void);
 
 #pragma code high_vector = 0x08 // force the following statement to start
-void high_interrupt (void) // at 0x08
+
+void high_interrupt(void) // at 0x08
 {
-	_asm
-	goto high_ISR
-	_endasm
+    _asm
+    goto high_ISR
+            _endasm
 }
 
 #pragma code low_vector = 0x18 //force the following statements to start
-void low_interrupt (void) // at 0x18
+
+void low_interrupt(void) // at 0x18
 {
-	_asm
-	goto low_ISR
-	_endasm
+    _asm
+    goto low_ISR
+            _endasm
 }
 
 /*****************************/
 
 #pragma code //return to the default code section
 #pragma interrupt high_ISR
-void high_ISR (void)
-{
-	if(INTCONbits.TMR0IF) //handle high-priority interrupts
-		{
-			OpenADC( ADC_FOSC_64 & ADC_RIGHT_JUST & ADC_1ANA , ADC_CH0 & ADC_INT_OFF , 0 );
-			ConvertADC();
-			while(BusyADC());
-			resultado= ReadADC();
 
-			OpenTimer0( TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_64 );
-			WriteTimer0( 57722 );
-			INTCONbits.TMR0IF=0;	
-		}
+void high_ISR(void) {
+    if (INTCONbits.TMR0IF) //handle high-priority interrupts
+    {
+        OpenADC(ADC_FOSC_64 & ADC_RIGHT_JUST & ADC_1ANA, ADC_CH0 & ADC_INT_OFF, 0);
+        ConvertADC();
+        while (BusyADC());
+        resultado = ReadADC();
+
+        OpenTimer0(TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_64);
+        WriteTimer0(57722);
+        INTCONbits.TMR0IF = 0;
+    }
 }
 
 
 #pragma interruptlow low_ISR
-void low_ISR (void)
-{
-	_asm // handle low-priority interrupts
-	retfie 0 // simply return
-	_endasm
+
+void low_ISR(void) {
+    _asm // handle low-priority interrupts
+    retfie 0 // simply return
+            _endasm
 }
 
 /*****************************/
@@ -67,38 +69,37 @@ unsigned char getc_usart(void) {
 /*********************************/
 
 
- void main (void)
-{
-	char c;
-  	char str[7] = "ECHO:x";
+void main(void) {
+    char c;
+    char str[7] = "ECHO:x";
 
-	RCONbits.IPEN = 1; // Enable priority interrupt
-	INTCON = 0b10100000;
+    RCONbits.IPEN = 1; // Enable priority interrupt
+    INTCON = 0b10100000;
 
-	TRISD = 0x00; // Configure PORTD for output
-	PORTD = 0b00000000; // turn off all LEDs initially
-	TRISB = 0b00000000;
-     PORTB = 0b00000000;
+    TRISD = 0x00; // Configure PORTD for output
+    PORTD = 0b00000000; // turn off all LEDs initially
+    TRISB = 0b00000000;
+    PORTB = 0b00000000;
 
-	//TMR0H=0x67;
-	//TMR0L=0x69;
-	//T0CON=0b10000110;
+    //TMR0H=0x67;
+    //TMR0L=0x69;
+    //T0CON=0b10000110;
 
-//    configure USART
+    //    configure USART
     OpenUSART(USART_TX_INT_OFF & USART_RX_INT_OFF & USART_ASYNCH_MODE & USART_EIGHT_BIT & USART_CONT_RX,
-			 129);
+            129);
 
 
-	OpenADC( ADC_FOSC_64 & ADC_RIGHT_JUST & ADC_1ANA , ADC_CH0 & ADC_INT_OFF , 0 );
-	OpenTimer0( TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_64 );
-	WriteTimer0( 57722 );
-	
+    OpenADC(ADC_FOSC_64 & ADC_RIGHT_JUST & ADC_1ANA, ADC_CH0 & ADC_INT_OFF, 0);
+    OpenTimer0(TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_64);
+    WriteTimer0(57722);
 
-	//CONFIGURAR AQUI AS INTERRUPÇÕES
 
-	while (1); // stay in an infinite loop
-	{
-		c = getc_usart();
+    //CONFIGURAR AQUI AS INTERRUPÇÕES
+
+    while (1); // stay in an infinite loop
+    {
+        c = getc_usart();
         if (c == 'l') {
             PORTB = 0b00000001;
         } else if (c == 'd') {
@@ -110,5 +111,5 @@ unsigned char getc_usart(void) {
         str[5] = c;
         putsUSART(str); // send string to USART
 
-	}
+    }
 }
