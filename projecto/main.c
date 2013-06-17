0f217ff91a14cfd28e0c5c04983949e90aca99c4
André Viana <sou_eu@crawler.com>
2013-06-17 11:54:27 +0100

    / ‘projecto/main.c’ - ‘projecto/main.err’ / ‘projecto/main.o’ - ‘projecto/projecto.cof’ - ‘projecto/projecto.hex’

--- projecto/main.c
+++ projecto/main.c
@@ -70,59 +70,41 @@ void low_ISR(void) {
 /*****************************/
 // Usart related code
 
-unsigned char getc_usart(void) {
-    while (!PIR1bits.RCIF);
-    PIR1bits.RCIF = 0;
-    return RCREG;
+unsigned char getc_usart(void)
+{
+	while (!PIR1bits.RCIF);	
+	PIR1bits.RCIF = 0;
+	return RCREG;
 }
 
 /*********************************/
 
 
-void main(void) {
-    char c;
-    char str[7]="ECHO:x\0";
 
-    RCONbits.IPEN = 1; // Enable priority interrupt
+
+void main(void)
+{
+	char c;
+	char str[7]="ECHO:x";
+
+	RCONbits.IPEN = 1; // Enable priority interrupt
     INTCON = 0b10100000;
 
-    TRISD = 0b00000000; // Configure PORTD for output
+	TRISD = 0b00000000; // Configure PORTD for output
     PORTD = 0b00000000; // turn off all LEDs initially
     TRISB = 0b00000010; // Configure PORTB for output except RB1
     PORTB = 0b00000000; // turn off all LEDs initially
 
-    //TMR0H=0x67;
-    //TMR0L=0x69;
-    //T0CON=0b10000110;
-	
+	OpenUSART (USART_TX_INT_OFF & USART_RX_INT_OFF & USART_ASYNCH_MODE & USART_EIGHT_BIT & USART_CONT_RX,129 );
 
-    //    configure USART
-    OpenUSART(USART_TX_INT_OFF &
-            USART_RX_INT_OFF &
-            USART_ASYNCH_MODE &
-            USART_EIGHT_BIT &
-            USART_CONT_RX,
-            129);
-
-
-    OpenADC(ADC_FOSC_64 & ADC_RIGHT_JUST & ADC_1ANA, ADC_CH0 & ADC_INT_OFF, 0);
+	OpenADC(ADC_FOSC_64 & ADC_RIGHT_JUST & ADC_1ANA, ADC_CH0 & ADC_INT_OFF, 0);
     OpenTimer0(TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_64);
     WriteTimer0(57722);
-
-
-    //CONFIGURAR AQUI AS INTERRUPÇÕES
-
-    while (1); // stay in an infinite loop
-    {
-        // ADC handler
-
-        ConvertADC();
-        while (BusyADC());
-        resultado = ReadADC();
-		
-		// Usart handler
-        c = getc_usart();
-        if (c == 'l') {
+	
+	while(1)
+		{
+		c = getc_usart();
+		if (c == 'l') {
             PORTB = 0b00000001;
         } else if (c == 'd') {
             PORTB = 0b00000000;
@@ -133,10 +115,12 @@ void main(void) {
         else if (c == 'f') {
             resultado = resultado - 50;
         }
-        
-        // get char from USART by polling method
-        str[5] = c;
-        putsUSART(str); // send string to USART
+		str[5]=c;
+		putsUSART( str );
 
-    }
-}
+		ConvertADC();
+        while (BusyADC());
+        resultado = ReadADC();
+
+		}
+}	
