eacaa2e02eb5700b1ac492ae6a580eb7ea179282
André Viana <sou_eu@crawler.com>
2013-06-20 23:59:00 +0100

    / ‘projecto/main.c’ + ‘projecto/main.err’ - ‘projecto/main.o’ - ‘projecto/projecto.cof’ - ‘projecto/projecto.hex’

--- projecto/main.c
+++ projecto/main.c
@@ -43,11 +43,12 @@ void high_ISR(void) {
 		
 		INTCON2bits.INTEDG0 = ~(INTCON2bits.INTEDG0);
 		PORTBbits.RB1 = 0;
+		PORTBbits.RD7 = 0;
 
 		//programar timer
 		//OpenTimer0(TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_256);
         //WriteTimer0(65359);
-		PORTDbits.RD7 = ~(PORTDbits.RD7 );
+		//PORTDbits.RD7 = ~(PORTDbits.RD7 );
 		
 		INTCONbits.INT0IF = 0;
 		
@@ -57,6 +58,7 @@ void high_ISR(void) {
 
         // Timer0 handler
 		PORTBbits.RB1 = 1;
+		PORTBbits.RD7 = 1;
 		
         OpenTimer0(TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_256);
         WriteTimer0(65359);
