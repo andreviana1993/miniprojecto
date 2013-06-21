a09224c809709d172a278c2bb0b174071b07783e
André Viana <sou_eu@crawler.com>
2013-06-21 17:01:33 +0100

    / ‘projecto/main.c’ - ‘projecto/main.o’ - ‘projecto/projecto.cof’ - ‘projecto/projecto.hex’ - ‘projecto/projecto.mcs’

--- projecto/main.c
+++ projecto/main.c
@@ -39,11 +39,12 @@ void low_interrupt(void) // at 0x18
 
 void high_ISR(void) {
     if (INTCONbits.INT0IF) {
+		INTCON2bits.INTEDG0 = ~(INTCON2bits.INTEDG0);
 		if (ligarResistencia){
 		OpenTimer0(TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_256);
 		WriteTimer0(65359);
 		
-		INTCON2bits.INTEDG0 = ~(INTCON2bits.INTEDG0);
+		
 		PORTBbits.RB1 = 0;
 		PORTDbits.RD7 = 0;
 
