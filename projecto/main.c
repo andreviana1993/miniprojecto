4e3ad30245e548ddb14d072af5b887d647ca52ce
André Viana <sou_eu@crawler.com>
2013-06-22 00:22:23 +0100

    / ‘projecto/main.c’ / ‘projecto/main.o’ - ‘projecto/projecto.cof’ - ‘projecto/projecto.hex’

--- projecto/main.c
+++ projecto/main.c
@@ -13,7 +13,7 @@ int contagem=0;
 
 void timer10us( void)
 {
-		WriteTimer0( 65486 );
+		WriteTimer0( 206 );
 		contagem++;
 }
 
@@ -116,7 +116,7 @@ void main(void) {
     ADCON0 = 0b00000001;
     ADCON1 = 0b00111110;
     ADCON2 = 0b10000110;
-    OpenTimer0(TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_1);
+    OpenTimer0(TIMER_INT_ON & T0_8BIT & T0_SOURCE_INT & T0_PS_1_1);
     //WriteTimer0( 6 );
 
     while (1) {
